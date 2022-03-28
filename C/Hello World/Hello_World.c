45b1ff9990b74f9955706988d0df5a0be371ce62
Charles <ckeer101@gmail.com>
2022-03-28 11:38:45 -0400

    Added first files and Hello World

---
+++ C/Hello World/Hello_World.c
@@ -0,0 +1,15 @@
+/*
+Designed to print "Hello World to terminal."
+*/
+
+//Include stdio to be able to use the printf function.
+#include <stdio.h>
+
+int main()
+ {
+    //Print "Hello World" to terminal.
+    printf("Hello World!");
+
+    return 0;
+    
+}
